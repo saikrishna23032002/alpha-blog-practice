7493fd645ad87319812fcbf7acc59fd95b0f1df1
saikrishna23032002 <saikrishnatv23@gmail.com>
2026-01-07 13:56:28 +0530

    home page and about pages are created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+  def home
+    render html: "this is home page"
+  end
 end

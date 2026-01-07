76978fd7bed44ffa45bfa9f04ae4f1df4edeff22
saikrishna23032002 <saikrishnatv23@gmail.com>
2026-01-07 13:59:09 +0530

    contact page is added

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   root "application#home"
 
   get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
